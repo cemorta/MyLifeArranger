753e917e53a64a4dd9564bd1561b3676f887c5ec
cemorta <cem.orta@hotmail.com>
2023-05-19 13:58:24 +0300

    Start the development of Timeline in Day View

---
+++ app/src/main/java/com/example/mylifearranger/Event.kt
@@ -0,0 +1,5 @@
+package com.example.mylifearranger
+
+import java.time.LocalTime
+
+data class Event(val name: String, val start: LocalTime, val end: LocalTime)
--- app/src/main/java/com/example/mylifearranger/MainActivity.kt
+++ app/src/main/java/com/example/mylifearranger/MainActivity.kt
@@ -11,15 +11,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -30,8 +35,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -39,11 +46,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
@@ -93,8 +105,69 @@ fun DayScreen(currentDate: LocalDate?) {
             Column {
                 WeekDaysRow(currentDate)
                 Divider()
+                TimelineView()
             }
         }
+    }
+}
+
+@Composable
+fun TimelineView() {
+    val scrollState = rememberScrollState()
+
+    // Create a list of hours to display in the timeline
+    val hours = List(24) { String.format("%02d:00", it) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 8.dp, bottom = 8.dp)
+            .verticalScroll(scrollState)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(60.dp) // Add space between items
+        ) {
+            for (hour in hours) {
+                Text(hour)
+            }
+        }
+        EventRows()
+    }
+}
+
+@Composable
+fun EventRows() {
+    Column {
+        Spacer(modifier = Modifier.height(returnClockPosition("01:00").dp))
+        EventRow("Event 1")
+    }
+}
+
+fun returnClockPosition(time: String): Float {
+    val initialPadding = 11
+    val paddingBetweenHours: Float = 81.5F
+    val paddingBetweenMinutes: Float = 1.35F
+
+    val hour = time.substring(0, 2).toInt()
+    val minutes = time.substring(3, 5).toInt()
+    val result: Float =
+        hour * paddingBetweenHours + initialPadding + minutes * paddingBetweenMinutes
+    return result
+}
+
+@Composable
+fun EventRow(eventName: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Red.copy(alpha = 0.5f))
+            .height(50.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(eventName, textAlign = TextAlign.Center, modifier = Modifier.weight(1f))
     }
 }
 
